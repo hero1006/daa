e28f14ddfee7fea819030046571da1a5fd1aaa13
hero1006 <ahirraobhagyshri6@gmail.com>
2025-06-23 20:56:17 +0530

    Update 2knapsack.py

--- 2knapsack.py
+++ 2knapsack.py
@@ -25,3 +25,32 @@ cap = 50
 total_profit, include_items = knapsack(wt, profit, cap)
 print("\n Total profit is:", total_profit)
 print("include items are:",include_items)
+
+#largets ratio
+def knapsack(wt, profit, cap):
+    items = [(i, wt[i], profit[i], wt[i]/profit[i]) for i in range(len(wt))]
+    items.sort(key = lambda x:x[3], reverse=True)
+
+    total_profit = 0
+    include_items = []
+
+    for i, wt, profit, ratio in items:
+        if cap >= wt:
+            include_items.append((i, 1))
+            total_profit = total_profit + profit
+            cap = cap - wt
+        
+        else:
+            frac = cap / wt
+            total_profit = total_profit + (frac*profit)
+            include_items.append((i, frac))
+
+    return total_profit, include_items
+
+wt = [30, 20, 10]
+profit = [60, 50, 40]
+cap = 50
+
+total_profit, include_items = knapsack(wt, profit, cap)
+print("\n Total profit is:", total_profit)
+print("include items are:",include_items)
